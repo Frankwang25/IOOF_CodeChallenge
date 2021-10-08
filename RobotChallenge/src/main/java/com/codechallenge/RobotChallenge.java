feb57609b55a56089a1b17aee12691202f22656c
Frankwang25 <gmfrankwang@gmail.com>
2021-10-09 09:24:31 +1100

    Commit for new command (RESTART)

--- RobotChallenge/src/main/java/com/codechallenge/RobotChallenge.java
+++ RobotChallenge/src/main/java/com/codechallenge/RobotChallenge.java
@@ -25,15 +25,15 @@ public class RobotChallenge {
 
 	public static void main(String[] args) {
 		System.out.println("Control Robot(s) with following acceptable commands (Case Sensitive):");
-		System.out
-				.println("PLACE X,Y,NORTH|SOUTH|EAST|WEST, MOVE, LEFT, RIGHT, REPORT, ROBOT <Number>, SHOW and EXIT;");
+		System.out.println("PLACE X,Y,NORTH|SOUTH|EAST|WEST, MOVE, LEFT, RIGHT, REPORT, ROBOT <Number>, SHOW, RESTART and EXIT;");
 		System.out.println("1 PLACE X,Y,NORTH|SOUTH|EAST|WEST: Place a new robot in a position with facing;");
 		System.out.println("2 MOVE: Move a robot to the next position which the robot is facing to;");
 		System.out.println("3 LEFT, RIGHT: Rotate robot in other facing;");
 		System.out.println("4 REPORT: Report the position of a robot with facing;");
 		System.out.println("5 ROBOT <Number>: Switch to different robot with specified number like ROBOT 2;");
 		System.out.println("6 SHOW: Present robot(s) in the table;");
-		System.out.println("7 EXIT: Exit application;");
+		System.out.println("7 RESTART: Restart to play;");
+		System.out.println("8 EXIT: Exit application;");
 		RobotChallenge rc = new RobotChallenge(System.in, System.out);
 		rc.controlRobot();
 	}
